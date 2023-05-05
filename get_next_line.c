5772040d7dd4d52c3150b4d1df7872e29ff99ccc
Wilson Santos <wilsontsantos1998@gmail.com>
2023-05-05 19:18:46 +0100

    .

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: wteles-d <wteles-d@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/27 15:18:27 by wteles-d          #+#    #+#             */
-/*   Updated: 2023/05/04 19:38:02 by wteles-d         ###   ########.fr       */
+/*   Updated: 2023/05/05 19:03:57 by wteles-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,18 +21,28 @@
 #include "get_next_line.h"
 
 int		ft_spec_strcpy(char *dst, const char *src, int i);
-char	*ft_strjoin(char const *s1, char const *s2);
+char	*ft_strjoin(char *s1, char *s2);
 char	*ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *s);
 char	*ft_read(int fd);
+char	*ft_extract_str(char *str);
+char	*ft_trim_str(char *str);
 
 char	*get_next_line(int fd)
 {
-	//static char	*mainsource;
+	static char	*mainsource;
 	char		*p;
-
-	p = ft_read(fd);
-	return (p);
+	char		*r;
+	
+	if (BUFFER_SIZE <= 0 || read(fd, NULL, 0) == -1)
+		return (NULL);
+	r = ft_read(fd);
+	p = ft_strjoin(mainsource, r);
+	free(r);
+	if (ft_strlen(p) == 0)
+		return (NULL);
+	mainsource = ft_trim_str(p);
+	return (ft_extract_str(p));
 }
 
 char	*ft_read(int fd)
@@ -45,9 +55,7 @@ char	*ft_read(int fd)
 	{
 		ret = read(fd, buf, BUFFER_SIZE);
 		buf[ret] = '\0';
-		printf("%s\n", buf);
 		joint = ft_strjoin(joint, buf);
-		printf("%s\n", joint);
 		i = 0;
 		while (joint[i])
 		{
@@ -61,10 +69,73 @@ char	*ft_read(int fd)
 	return (joint);
 }
 
+char	*ft_extract_str(char *str)
+{
+	int	i = 0;
+	char *dup;
+	if (!str)
+		return (NULL);
+	while (str && str[i] && str[i] != '\n')
+		i++;
+	dup = (char *)malloc((i + 2) * sizeof(char));
+	if (!dup)
+		return (NULL);
+	return (ft_strlcpy(dup, str, i + 2));
+}
+
+char	*ft_trim_str(char *str)
+{
+	int i = 0;
+	int j = 0;
+	int	trimsize = 0;
+	char *trim;
+	if (!str)
+		return (NULL);
+	while (str && str[i] && str[i] != '\n')
+		i++;
+	if (str)
+		i++;
+	trimsize = (ft_strlen(str)) - i;
+	trim = (char *)malloc(trimsize + 1 * sizeof(char));
+	if (!trim)
+		return (NULL);
+	while (str && str[i])
+	{
+		trim[j] = str[i];
+		i++;
+		j++;
+	}
+	trim[j] = '\0';
+	return (trim);
+}
+
 int	main(void)
 {
 	int	fd = open("/nfs/homes/wteles-d/Wilson/get_next_line/testfd.txt", O_RDWR);
-	printf("FINAL RETURN 1 -> %s\n", get_next_line(fd));
+	char *p;
+	
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
+	printf("-> %s\n", p = get_next_line(fd));
+	free(p);
 }
 
 int	ft_spec_strcpy(char *dst, const char *src, int i)
@@ -81,20 +152,19 @@ int	ft_spec_strcpy(char *dst, const char *src, int i)
 	return (i);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
+char	*ft_strjoin(char *s1, char *s2)
 {
 	int		i;
-	char	*p;
+	char	*p = 0;
 
 	i = 0;
-	if (!s2)
-		return (NULL);
-	p = (char *)calloc(ft_strlen(s1) + ft_strlen(s2) + 1, 1);
+	p = (char *)calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
 	if (p == NULL)
 		return (p);
 	i = ft_spec_strcpy(p, s1, i);
 	i = ft_spec_strcpy(p, s2, i);
 	p[i] = '\0';
+	free(s1);
 	return (p);
 }
 
